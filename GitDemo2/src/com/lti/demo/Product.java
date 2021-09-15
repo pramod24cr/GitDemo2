fc1a60ebe421241fc79a14544aa2b8f998fd4273
pramod24cr <pramod24cr@gmail.com>
2021-09-15 15:52:51 +0530

    First Commit

---
+++ GitDemo2/src/com/lti/demo/Product.java
@@ -0,0 +1,34 @@
+package com.lti.demo;
+//GitDemo2
+public class Product 
+{
+	private int prdID;
+	private String prdName;
+	private  int prdCost;
+	
+
+	Product (int prdID, String prdName, int prdCost)
+	{	super();
+		this.prdID=prdID;
+		this.prdName=prdName;
+		this.prdCost=prdCost;
+	}
+	public int getPrdID() {
+		return prdID;
+	}
+	public void setPrdID(int prdID) {
+		this.prdID = prdID;
+	}
+	public String getPrdName() {
+		return prdName;
+	}
+	public void setPrdName(String prdName) {
+		this.prdName = prdName;
+	}
+	public int getPrdCost() {
+		return prdCost;
+	}
+	public void setPrdCost(int prdCost) {
+		this.prdCost = prdCost;
+	}
+}
---
+++ GitDemo2/src/com/lti/demo/UserApp.java
@@ -0,0 +1,13 @@
+package com.lti.demo;
+//Demo2
+public class UserApp 
+{
+	public static void main(String[] args) 
+	{
+		Product p = new Product(101,"Mobile",500);
+		System.out.println(p);
+		System.out.println("Print Message");
+		System.out.println("Print Message");
+		System.out.println("Print Message");
+	}
+}
